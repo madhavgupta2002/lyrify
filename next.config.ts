6af463b60c2317f841b73ef56e22fa8e54c9f10b
Madhav Gupta <mgups2021@gmail.com>
2024-12-22 20:32:42 +0530

    test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb'
+    },
+    responseLimit: false
+  }
 };
 
 export default nextConfig;
